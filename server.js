77efa5c75a4a081f3ea5d4983f8c29dc5071c99e
Seon Kim <jeongahkim8174@gmail.com>
2022-07-17 18:18:33 -0700

    form now gets the json data back after posted

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
+const { urlencoded } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const e = require('express');
+const urlencodedParser = bodyParser.urlencoded({extended: true});
 
 app.set('view engine','ejs');
 app.set('views','views')
@@ -15,7 +19,27 @@ app.get('/home',(req,res)=>{
 app.get('/signUp',(req,res) =>{
     res.render('signUp');
 })
+app.get('/hi',(req,res) =>{
+    res.render('start.ejs')
+})
 
+app.post('/signUp',urlencodedParser,(req,res) =>{
+    const username = req.body.Username;
+    const password = req.body.Password;
+    const confirmPassword = req.body.confirmPassword;
+    console.log(username,password,confirmPassword);
+
+    //checks the required fields
+    if(username.length >= 6 && username.length <= 16 && password.length >= 6 && password.length <= 20 && confirmPassword.length >= 6 && confirmPassword.length <= 20 && password===confirmPassword){
+        //submits if the form meets requirements
+        console.log('successfully submitted');
+        res.redirect('/hi')
+    }
+    else {
+        console.log('error has occurred')
+    }
+
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`listening on PORT ${PORT}`);
